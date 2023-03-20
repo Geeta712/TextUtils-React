4ccc4c84e1cf19069b6c01823ff77978fedb4b6a
Geeta712 <128187352+Geeta712@users.noreply.github.com>
2023-03-20 16:24:49 -0400

    Updates

---
+++ precache-manifest.8e9a7f6d87d70ed752f62c029908378c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e19116431c27b266494e",
+    "url": "/Textutils-React/static/css/main.265f4476.chunk.css"
+  },
+  {
+    "revision": "e19116431c27b266494e",
+    "url": "/Textutils-React/static/js/main.e1911643.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Textutils-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Textutils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "daf74f3886ef26676126",
+    "url": "/Textutils-React/static/js/runtime~main.daf74f38.js"
+  },
+  {
+    "revision": "465a37169bcd426d834fa8637138a5c4",
+    "url": "/Textutils-React/index.html"
+  }
+];
